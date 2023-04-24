e70594c66d8b201f6ff3e43684c105ec2790c7f7
j$ <55032731+60fov@users.noreply.github.com>
2023-04-23 22:06:02 -0500

    cookiesss

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,22 +1,5 @@
-import { serialize, CookieSerializeOptions } from 'cookie'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-export const setCookie = (
-  res: NextApiResponse,
-  name: string,
-  value: unknown,
-  options: CookieSerializeOptions = {}
-) => {
-  const stringValue =
-    typeof value === 'object' ? 'j:' + JSON.stringify(value) : String(value)
-
-  if (typeof options.maxAge === 'number') {
-    options.expires = new Date(Date.now() + options.maxAge * 1000)
-  }
-
-  res.setHeader('Set-Cookie', serialize(name, stringValue, options))
-}
+import { setCookie } from '~/util/cookies'
 
 export default function handler(
   req: NextApiRequest,
